39df4475dc93468acf514c8e0eea43fc01b6ead5
Nahid2194 <nahidjc80@gmail.com>
2021-02-09 21:06:24 +0600

    first activity completed

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.quiz_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+RadioGroup myRadioGroup;
+RadioButton myRadioButton;
+int mark;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myRadioGroup = findViewById(R.id.radioGroupId);
+    }
+
+    public void submitFunction(View view) {
+        int selectedRadioButtonId = myRadioGroup.getCheckedRadioButtonId();
+        myRadioButton = findViewById(selectedRadioButtonId);
+        if(myRadioButton.getText().toString().equals("Dhaka")){
+         mark=5;
+        }
     }
 }
